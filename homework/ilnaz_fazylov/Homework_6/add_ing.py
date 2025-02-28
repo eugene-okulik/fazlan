8a68861d9062be5004a7cdef38f1dddad812d21e
admin <test@list.com>
2025-02-28 23:29:20 +0500

    homework_6 done

---
+++ homework/ilnaz_fazylov/Homework_6/add_ing.py
@@ -0,0 +1,14 @@
+def update_text(text):
+    lst = text.split()
+    result = ''
+
+    for word in lst:
+        if '.' in word or ',' in word:
+            result = result + word[:-1] + 'ing' + word[-1] + ' '
+        else:
+            result = result + word + 'ing' + ' '
+
+    return result
+
+print(update_text(('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. '
+        'Integer urna nisl, facilisis vitae semper at, dignissim vitaae libero')))
