ff05c5de235331d6dceb1a69626eb3a1d7f782f5
admin <test@list.com>
2025-05-07 23:02:15 +0500

    added test_add_item_to_compare

--- homework/ilnaz_fazylov/Homework_24/test_adv_2.py
+++ homework/ilnaz_fazylov/Homework_24/test_adv_2.py
@@ -22,46 +22,73 @@ def accept_cookie(driver):
     accept_btn.click()
 
 
-def move_to_item_and_click(driver):
+def move_to_item(driver):
     wait = WebDriverWait(driver, 10)
-    items = driver.find_elements(By.XPATH, '//*[@class="item product product-item"]')
-    for elem in range(1, len(items)):
-        item = wait.until(
-            EC.visibility_of_element_located(
-                (By.XPATH, f'//*[@class="item product product-item"][{elem}]')))
-        add_to_cart_btn = wait.until(
-            EC.element_to_be_clickable(
-                (By.XPATH, f"(//button[@title='Add to Cart'])[{elem}]")))
-        ActionChains(driver).move_to_element(item).click(add_to_cart_btn).perform()
-        is_item = check_item_in_cart(driver)
-        if is_item:
-            item_name = driver.find_element(By.XPATH, f'(//*[@class="product name product-item-name"])[{elem}]').text
-            return item_name
+    item = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@class="item product product-item"][1]')))
+    item_name = driver.find_element(By.XPATH, '(//*[@class="product name product-item-name"])[1]').text
+    ActionChains(driver).move_to_element(item).perform()
+    return item_name
 
 
-def check_item_in_cart(driver):
+def click_button_to_compare(driver):
     wait = WebDriverWait(driver, 10)
-    is_item = 1
-    try:
-        error = wait.until(
-            EC.visibility_of_element_located((By.XPATH, "//*[contains(text(), 'The requested qty')]"))).text
-        if error:
-            driver.back()
-            is_item = 0
-    except Exception:
-        return is_item
+    add_to_compare_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "(//a[@title='Add to Compare'])[1]")))
+    add_to_compare_btn.click()
 
 
-def check_add_item_in_cart(driver, item_name):
+def check_add_item_to_compare(driver, item_name):
     wait = WebDriverWait(driver, 10)
-    success_text = wait.until(EC.visibility_of_element_located((By.XPATH, "//*[contains(text(), 'You added')]"))).text
-    counter = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'counter-number')))
-    counter.click()
-    item_in_cart = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@class="product-item-name"]'))).text
-    assert success_text == f'You added {item_name} to your shopping cart.', 'Text is incorrect'
-    assert item_name == item_in_cart, f"Expected '{item_name}' in cart, but got '{item_in_cart}'"
+    success_text = wait.until(
+        EC.visibility_of_element_located((By.XPATH, "//*[contains(text(), 'You added product')]"))).text
+    assert success_text == f'You added product {item_name} to the comparison list.', 'Text is incorrect'
+    compare_item = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@class="product-item-name"]'))).text
+    assert item_name == compare_item, f"Expected '{item_name}' in cart, but got '{compare_item}'"
 
 
-def test_add_item_in_cart(driver):
-    item_name = move_to_item_and_click(driver)
-    check_add_item_in_cart(driver, item_name)
+def test_add_item_to_compare(driver):
+    item_name = move_to_item(driver)
+    click_button_to_compare(driver)
+    check_add_item_to_compare(driver, item_name)
+
+# def move_to_item_and_click(driver):
+#     wait = WebDriverWait(driver, 10)
+#     items = driver.find_elements(By.XPATH, '//*[@class="item product product-item"]')
+#     for elem in range(1, len(items)):
+#         item = wait.until(
+#             EC.visibility_of_element_located(
+#                 (By.XPATH, f'//*[@class="item product product-item"][{elem}]')))
+#         add_to_cart_btn = wait.until(
+#             EC.element_to_be_clickable(
+#                 (By.XPATH, f"(//button[@title='Add to Cart'])[{elem}]")))
+#         ActionChains(driver).move_to_element(item).click(add_to_cart_btn).perform()
+#         is_item = check_item_in_cart(driver)
+#         if is_item:
+#             item_name = driver.find_element(By.XPATH, f'(//*[@class="product name product-item-name"])[{elem}]').text
+#             return item_name
+#
+#
+# def check_item_in_cart(driver):
+#     wait = WebDriverWait(driver, 10)
+#     is_item = 1
+#     try:
+#         error = wait.until(
+#             EC.visibility_of_element_located((By.XPATH, "//*[contains(text(), 'The requested qty')]"))).text
+#         if error:
+#             driver.back()
+#             is_item = 0
+#     except Exception:
+#         return is_item
+#
+#
+# def check_add_item_in_cart(driver, item_name):
+#     wait = WebDriverWait(driver, 10)
+#     success_text = wait.until(EC.visibility_of_element_located((By.XPATH, "//*[contains(text(), 'You added')]"))).text
+#     counter = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'counter-number')))
+#     counter.click()
+#     item_in_cart = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@class="product-item-name"]'))).text
+#     assert success_text == f'You added {item_name} to your shopping cart.', 'Text is incorrect'
+#     assert item_name == item_in_cart, f"Expected '{item_name}' in cart, but got '{item_in_cart}'"
+#
+# def test_add_item_in_cart(driver):
+#     item_name = move_to_item_and_click(driver)
+#     check_add_item_in_cart(driver, item_name)
