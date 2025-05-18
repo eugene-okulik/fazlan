8f2b698c74773dfd3fc0abb0e62e558fd516e308
admin <test@list.com>
2025-05-18 19:25:32 +0500

    homework_32 done

---
+++ homework/ilnaz_fazylov/Homework_32/decor.py
@@ -0,0 +1,20 @@
+import time
+import requests
+
+def my_decorator(func):
+    def wrapper():
+        time_start = time.time()
+        print(f'Определим время начала выполнения функции {round(time_start, 2)}')
+        func()
+        time_end = time.time()
+        print(f'Определим время завершения выполнения функции {round(time_end, 2)}')
+        print(f'Определим время выполнения функции {round((time_end - time_start), 2)}')
+    return wrapper
+
+@my_decorator
+def get_url():
+    print('Выполняем функцию')
+    response = requests.get('https://www.google.ru/')
+    return response
+
+get_url()
