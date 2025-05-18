0837e9f9caad68aa3aeed99bd76a5cc51c080058
admin <test@list.com>
2025-05-18 19:27:28 +0500

    fixed linter error

--- homework/ilnaz_fazylov/Homework_32/decor.py
+++ homework/ilnaz_fazylov/Homework_32/decor.py
@@ -1,6 +1,7 @@
 import time
 import requests
 
+
 def my_decorator(func):
     def wrapper():
         time_start = time.time()
@@ -9,12 +10,15 @@ def my_decorator(func):
         time_end = time.time()
         print(f'Определим время завершения выполнения функции {round(time_end, 2)}')
         print(f'Определим время выполнения функции {round((time_end - time_start), 2)}')
+
     return wrapper
+
 
 @my_decorator
 def get_url():
     print('Выполняем функцию')
     response = requests.get('https://www.google.ru/')
     return response
+
 
 get_url()
