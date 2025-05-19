ac68012ea74af73fd28d7a058d9f189fa4c4617f
admin <test@list.com>
2025-05-20 00:16:17 +0500

    fixed linter error

--- homework/ilnaz_fazylov/Homework_33/test_demo.py
+++ homework/ilnaz_fazylov/Homework_33/test_demo.py
@@ -1,9 +1,11 @@
 import requests
 
+
 def test_google():
     response = requests.get("https://www.google.com")
     assert response.status_code == 200
     print("Test passed: Google is reachable.")
+
 
 if __name__ == "__main__":
     test_google()
